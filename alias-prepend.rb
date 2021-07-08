9c97c4d02f941ad560679bfb431279d9d028547a
Hamza Khan <h.akhan@outlook.com>
2021-07-08 09:24:16 -0700

    Added comments to alias-prepend code

--- alias-prepend.rb
+++ alias-prepend.rb
@@ -1,3 +1,12 @@
+# Class definition that exists in core code/gem
+class HttpClient
+    def Get(url)
+        puts "GET #{url}"
+    end
+end
+
+# Another gem or client code tries to decorate the core code with retry capability
+# Done using module#prepend
 module HttpRetryWrapper
     def Get(url)
         puts "Invoking GET with Retry"
@@ -5,22 +14,22 @@ module HttpRetryWrapper
     end
 end
 
-class HttpClient
-    def Get(url)
-        puts "GET #{url}"
-    end
-end
-
+# In method resolution chain the following would be called in order after mixin (prepend) is executed:
+# 1. module.HttpRetryWrapper.Get
+# 2. class.HttpClient.Get
 HttpClient.prepend(HttpRetryWrapper)
 
-class HttpClient
 
+# HttpClient redefinition will instrument (decorate) the core code, which at this moment in runtime 
+# has already been decorated by the module.HttpRetryWrapper with prepend,
+# This instrumentation code uses alias_method rather than module#prepend to accomplish that
+class HttpClient
     alias_method :GetNotInstrumented, :Get
     
     def GetInstrumented(url)
-        puts "Time before GET: "
+        puts "Starting span GET"
         GetNotInstrumented(url)
-        puts "Time after GET: "
+        puts "Ending span GET"
     end
 
     alias_method :Get, :GetInstrumented  
